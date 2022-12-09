3c42cbe00ea20c0ffebf7eb3b4b010c7a1ece294
rahul3090500 <76912944+rahul309050@users.noreply.github.com>
2022-12-09 19:49:11 +0530

    changes

---
+++ .next/server/middleware-react-loadable-manifest.js
@@ -0,0 +1,1 @@
+self.__REACT_LOADABLE_MANIFEST={"davinci.js -> @lottiefiles/lottie-player":{"id":"davinci.js -> @lottiefiles/lottie-player","files":["static/chunks/node_modules_lottiefiles_lottie-player_dist_lottie-player_esm_js.js"]}}
